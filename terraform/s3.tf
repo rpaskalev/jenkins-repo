fb666c1ab34467c91d8ecae57f5ce9b97c519c5b
rpaskalev <rpaskalev@lomixtech.com>
2022-04-02 19:47:32 -0400

    test4

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,7 @@
 
 resource "aws_s3_bucket" "iqies_my_first_resourse" {
   bucket = "${var.bucket_name}-${data.aws_region.current.id}" #-${count.index}"
-  acl    = var.acl_type
+  #acl    = var.acl_type
 
   tags = {
     Name        = var.bucket_name

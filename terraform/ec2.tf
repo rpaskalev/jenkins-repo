6fe070f00ba65e0e246de8daad76723e1c70cd5d
rpaskalev <rpaskalev@lomixtech.com>
2022-04-10 12:57:17 -0400

    prep1

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,50 +1,50 @@
-#this resource will create a separate ec2 instance in th edefault VPC. It will error is a default VPC doesnt exist. 
+# #this resource will create a separate ec2 instance in th edefault VPC. It will error is a default VPC doesnt exist. 
 
-resource "aws_instance" "web" {
-  ami = data.aws_ami.example.image_id #var.ami_id #"ami-02e136e904f3da870"
+# resource "aws_instance" "web" {
+#   ami = data.aws_ami.example.image_id #var.ami_id #"ami-02e136e904f3da870"
 
-  subnet_id                   = aws_subnet.my_subnet_1.id
-  associate_public_ip_address = true
-  # below we have 2 ways to add user data - directly or using the "file" function:
-  # user_data = <<EOF
-  # #!/bin/bash
-  # sudo yum update -y
-  # sudo yum install -y httpd
-  # sudo systemctl start httpd.service
-  # # sudo systemctl enable httpd.service
-  # sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
-  # EOF                   
+#   subnet_id                   = aws_subnet.my_subnet_1.id
+#   associate_public_ip_address = true
+#   # below we have 2 ways to add user data - directly or using the "file" function:
+#   # user_data = <<EOF
+#   # #!/bin/bash
+#   # sudo yum update -y
+#   # sudo yum install -y httpd
+#   # sudo systemctl start httpd.service
+#   # # sudo systemctl enable httpd.service
+#   # sudo echo "<h1> At $(hostname -f) </h1>" > /var/www/html/index.html
+#   # EOF                   
 
-  user_data = file("userdata.sh")
+#   user_data = file("userdata.sh")
 
-  ebs_optimized = var.ebs_optimized
+#   ebs_optimized = var.ebs_optimized
 
-  key_name      = "rady_key_new"
-  instance_type = var.instance_type
+#   key_name      = "rady_key_new"
+#   instance_type = var.instance_type
 
-  # Security group must be declared in the network_interface_id block if we are adding that parameter.
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
-  tags                   = var.ec2_tags
-}
+#   # Security group must be declared in the network_interface_id block if we are adding that parameter.
+#   vpc_security_group_ids = [aws_security_group.allow_all.id]
+#   tags                   = var.ec2_tags
+# }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.my_vpc.id
+# }
 
-resource "aws_route_table" "training" {
-  vpc_id = aws_vpc.my_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
+# resource "aws_route_table" "training" {
+#   vpc_id = aws_vpc.my_vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+# }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.my_subnet_1.id
-  route_table_id = aws_route_table.training.id
-}
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.my_subnet_1.id
+#   route_table_id = aws_route_table.training.id
+# }
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.my_subnet_2.id
-  route_table_id = aws_route_table.training.id
-}
+# resource "aws_route_table_association" "b" {
+#   subnet_id      = aws_subnet.my_subnet_2.id
+#   route_table_id = aws_route_table.training.id
+# }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,12 +1,137 @@
 
 
-resource "aws_s3_bucket" "iqies_my_first_resourse" {
-  bucket = "${var.bucket_name}-${data.aws_region.current.id}" #-${count.index}"
-  #acl    = var.acl_type
+# resource "aws_s3_bucket" "iqies_my_first_resourse" {
+#   bucket = "${var.bucket_name}-${data.aws_region.current.id}" #-${count.index}"
+#   #acl    = var.acl_type
 
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
-  }
+#   tags = {
+#     Name        = var.bucket_name
+#     Environment = var.environment
+#   }
+# }
+
+
+resource "random_pet" "petname" {
+  length    = 3
+  separator = "-"
 }
 
+
+resource "aws_s3_bucket" "dev_bucket" {
+  bucket = "development-${random_pet.petname.id}-${data.aws_region.current.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::development-${random_pet.petname.id}-${data.aws_region.current.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "dev" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+
+}
+
+resource "aws_s3_bucket" "prod_bucket" {
+  bucket = "production-${random_pet.petname.id}-${data.aws_region.current.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::production-${random_pet.petname.id}-${data.aws_region.current.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "prod" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.prod_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket" "qa_bucket" {
+  bucket = "qa-${random_pet.petname.id}-${data.aws_region.current.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::qa-${random_pet.petname.id}-${data.aws_region.current.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "qa" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.qa_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+}

1e83375d4a974a6586559c82c65ee6d5e10fb35f
rpaskalev <68789012+rpaskalev@users.noreply.github.com>
2022-10-31 18:19:44 -0400

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "dev_bucket" {
-  bucket = "development-rady-bucket-ziyotek-${data.aws_region.current.id}"
-
+  bucket = "radyzone.click"
+acl    = "public-read"
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -12,7 +12,9 @@ resource "aws_s3_bucket" "dev_bucket" {
             "Action": [
                 "s3:GetObject"
             ],
-            "Resource": "*"
+            "Resource": [
+              "arn:aws:s3:::radyzone.click/*"
+            ]
         }
     ]
 }
@@ -27,84 +29,11 @@ EOF
 }
 
 resource "aws_s3_bucket_object" "dev" {
+  acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.dev_bucket.id
   content      = file("../assets/index.html")
   content_type = "text/html"
 
 }
-
-resource "aws_s3_bucket" "prod_bucket" {
-  bucket = "production-${random_pet.petname.id}-${data.aws_region.current.id}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::production-${random_pet.petname.id}-${data.aws_region.current.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-  }
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_object" "prod" {
-  key          = "index.html"
-  bucket       = aws_s3_bucket.prod_bucket.id
-  content      = file("../assets/index.html")
-  content_type = "text/html"
-}
-
-
-resource "aws_s3_bucket" "qa_bucket" {
-  bucket = "qa-${random_pet.petname.id}-${data.aws_region.current.id}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::qa-${random_pet.petname.id}-${data.aws_region.current.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-  }
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_object" "qa" {
-  key          = "index.html"
-  bucket       = aws_s3_bucket.qa_bucket.id
-  content      = file("../assets/index.html")
-  content_type = "text/html"
-}
+  
